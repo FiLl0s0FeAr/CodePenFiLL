0130f67e238d44a32c42f22722dd30c60b62ce45
FiLl0s0FeAr <vivasu5@ukr.net>
2022-08-08 22:33:50 +0300

    add styles for main log and reg pages

--- src/pages/Reg.js
+++ src/pages/Reg.js
@@ -1,23 +1,60 @@
 import React, { useState } from "react";
 
+import "../styles/Login.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+
 export const Reg = ({ Reg }) => {
 	const [login, setLogin] = useState("");
 	const [password, setPassword] = useState("");
 	const [passwordValid, setPasswordValid] = useState("");
 	return (
-		<div className='login'>
-			<h3>Registration</h3>
-			<input className='username' type='login' placeholder='Username' value={login} onChange={e => setLogin(e.target.value)}/>
-			<input className='password' type='password' placeholder='Password' value={password} onChange={e => setPassword(e.target.value)}/>
-			<input className='password-confirm' type='password' placeholder='Confinm Password' value={passwordValid} onChange={e => setPasswordValid(e.target.value)}/>
-
-			<button type='submit' className='button-submit' disabled={!login || !password || password !== passwordValid} onClick={() => Reg(login, password)}>
-				Remember me.{" "}
-			</button>
-
-			<button className='main_page'>
-				<a href='/'>Main page</a>
-			</button>
+		<div className="Auth-form-container">
+			<form className="Auth-form">
+				<div className="Auth-form-content">
+					<h3 className="Auth-form-title">Sign Up</h3>
+					<div className="form-group mt-3">
+						<label>Username</label>
+						<input
+							type="login"
+							className="form-control mt-1"
+							placeholder="Enter username"
+							value={login}
+							onChange={e => setLogin(e.target.value)}
+						/>
+					</div>
+					<div className="form-group mt-3">
+						<label>Password</label>
+						<input
+							type="password"
+							className="form-control mt-1"
+							placeholder="Enter password"
+							value={password}
+							onChange={e => setPassword(e.target.value)}
+						/>
+					</div>
+					<div className="form-group mt-3">
+						<label>Password</label>
+						<input
+							type="password"
+							className="form-control mt-1"
+							placeholder="Confinm Password"
+							onChange={e => setPasswordValid(e.target.value)}
+						/>
+					</div>
+					<div className="d-grid gap-2 mt-3">
+						<button type="submit" className="btn btn-primary" disabled={!login || !password || password !== passwordValid} onClick={() => Reg(login, password)}>
+							Submit
+						</button>
+					</div>
+					<div className="d-grid gap-2 mt-3">
+						<a href='/'>
+							<button type="button" className="btn btn-primary main-page">
+								Main page
+							</button>
+						</a>
+					</div>
+				</div>
+			</form>
 		</div>
-	);
+	)
 };
