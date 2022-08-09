446211737cc7fce72b97ad96ed41246443f789bc
FiLl0s0FeAr <vivasu5@ukr.net>
2022-08-09 21:37:56 +0300

    fix Reg form

--- src/pages/Reg.js
+++ src/pages/Reg.js
@@ -1,49 +1,29 @@
 import React, { useState } from "react";
 
-import "../styles/Login.css"
-import "bootstrap/dist/css/bootstrap.min.css"
-
 export const Reg = ({ Reg }) => {
 	const [login, setLogin] = useState("");
 	const [password, setPassword] = useState("");
 	const [passwordValid, setPasswordValid] = useState("");
 	return (
-		<div className="Auth-form-container">
-			<form className="Auth-form">
+		<>
+			<div className="Auth-form-container">
 				<div className="Auth-form-content">
 					<h3 className="Auth-form-title">Sign Up</h3>
 					<div className="form-group mt-3">
 						<label>Username</label>
-						<input
-							type="login"
-							className="form-control mt-1"
-							placeholder="Enter username"
-							value={login}
-							onChange={e => setLogin(e.target.value)}
-						/>
+						<input type="login" className="form-control mt-1" placeholder="Enter username" value={login} onChange={e => setLogin(e.target.value)} />
 					</div>
 					<div className="form-group mt-3">
 						<label>Password</label>
-						<input
-							type="password"
-							className="form-control mt-1"
-							placeholder="Enter password"
-							value={password}
-							onChange={e => setPassword(e.target.value)}
-						/>
+						<input type="password" className="form-control mt-1" placeholder="Enter password" value={password} onChange={e => setPassword(e.target.value)} />
 					</div>
 					<div className="form-group mt-3">
-						<label>Password</label>
-						<input
-							type="password"
-							className="form-control mt-1"
-							placeholder="Confinm Password"
-							onChange={e => setPasswordValid(e.target.value)}
-						/>
+						<label>Confinm Password</label>
+						<input type="password" className="form-control mt-1" placeholder="Confinm Password" value={passwordValid} onChange={e => setPasswordValid(e.target.value)} />
 					</div>
 					<div className="d-grid gap-2 mt-3">
-						<button type="submit" className="btn btn-primary" disabled={!login || !password || password !== passwordValid} onClick={() => Reg(login, password)}>
-							Submit
+						<button type='submit' className="btn btn-primary main-page" disabled={!login || !password || password !== passwordValid} onClick={() => Reg(login, password)}>
+							Submit.{" "}
 						</button>
 					</div>
 					<div className="d-grid gap-2 mt-3">
@@ -54,7 +34,7 @@ export const Reg = ({ Reg }) => {
 						</a>
 					</div>
 				</div>
-			</form>
-		</div>
+			</div>
+		</>
 	)
 };
