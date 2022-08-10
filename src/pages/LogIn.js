156427beb412a013d5ac72c3ab5422e4cf35a733
FiLl0s0FeAr <vivasu5@ukr.net>
2022-08-10 09:46:29 +0300

    remove form from login

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -9,43 +9,41 @@ export const LogIn = ({ onLogin }) => {
 
 	return (
 		<div className="Auth-form-container">
-			<form className="Auth-form">
-				<div className="Auth-form-content">
-					<h3 className="Auth-form-title">Sign In</h3>
-					<div className="form-group mt-3">
-						<label>Username</label>
-						<input
-							type="login"
-							className="form-control mt-1"
-							placeholder="Enter username"
-							value={login}
-							onChange={e => setLogin(e.target.value)}
-						/>
-					</div>
-					<div className="form-group mt-3">
-						<label>Password</label>
-						<input
-							type="password"
-							className="form-control mt-1"
-							placeholder="Enter password"
-							value={password}
-							onChange={e => setPassword(e.target.value)}
-						/>
-					</div>
-					<div className="d-grid gap-2 mt-3">
-						<button type="submit" className="btn btn-primary" disabled={!login || !password} onClick={() => onLogin(login, password)}>
-							Submit
-						</button>
-					</div>
-					<div className="d-grid gap-2 mt-3">
-						<a href='/'>
-							<button type="button" className="btn btn-primary main-page">
-								Main page
-							</button>
-						</a>
-					</div>
+			<div className="Auth-form-content">
+				<h3 className="Auth-form-title">Sign In</h3>
+				<div className="form-group mt-3">
+					<label>Username</label>
+					<input
+						type="login"
+						className="form-control mt-1"
+						placeholder="Enter username"
+						value={login}
+						onChange={e => setLogin(e.target.value)}
+					/>
 				</div>
-			</form>
+				<div className="form-group mt-3">
+					<label>Password</label>
+					<input
+						type="password"
+						className="form-control mt-1"
+						placeholder="Enter password"
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+					/>
+				</div>
+				<div className="d-grid gap-2 mt-3">
+					<button type="submit" className="btn btn-primary" disabled={!login || !password} onClick={() => onLogin(login, password)}>
+						Submit
+					</button>
+				</div>
+				<div className="d-grid gap-2 mt-3">
+					<a href='/'>
+						<button type="button" className="btn btn-primary main-page">
+							Main page
+						</button>
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 };
